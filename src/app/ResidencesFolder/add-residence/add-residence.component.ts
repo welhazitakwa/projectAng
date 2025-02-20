ca01b8af0d210bda5155b673c13f5fd4fdd5a2d3
welhazitakwa <welhazitakwa@gmail.com>
2025-02-20 11:49:41 +0100

    workshop forms

--- src/app/ResidencesFolder/add-residence/add-residence.component.ts
+++ src/app/ResidencesFolder/add-residence/add-residence.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Residence } from 'src/app/Core/Model/residence';
 
 @Component({
   selector: 'app-add-residence',
   templateUrl: './add-residence.component.html',
-  styleUrls: ['./add-residence.component.css']
+  styleUrls: ['./add-residence.component.css'],
 })
 export class AddResidenceComponent {
+  addForm = new FormGroup({
+    id: new FormControl(0),
+    name: new FormControl('', Validators.required),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+    ]),
+    image: new FormControl(''),
+    status: new FormControl(''),
+  });
 
+  constructor( private Rout: Router) {}
+  get name() {
+    return this.addForm.get('name');
+  }
+
+  get address() {
+    return this.addForm.get('address');
+  }
+
+  R!: Residence;
+  SaveResidence(F: FormGroup) {
+    this.R = { ...F.value };
+    console.log(this.R);
+    this.resServ.addResidence(this.R);
+    this.Rout.navigate(['/residences']);
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ResidencesComponent } from './ResidencesFolder/residences/residences.component';
+import { ResidenceDetailsComponent } from './ResidencesFolder/residence-details/residence-details.component';
 
 const routes: Routes = [
   { path: 'Home', component: MainComponent },
   { path: 'Residences', component: ResidencesComponent },
+  { path: 'residenceD/:param', component: ResidenceDetailsComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
